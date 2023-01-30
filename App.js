0f5fdd2c65771e5471666048e0f00cc4c3795025
Fernando castro <fernandocastro77@yahoo.com>
2023-01-30 14:26:15 -0800

    finished making the header to the app

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
-// In App.js in a new project
-
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Homescreen from './screens/Homescreen';
-
-
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, Text, View } from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
 
 
 const Stack = createNativeStackNavigator();
 
-function App() {
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Homescreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <TailwindProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </TailwindProvider>
   );
 }
 
-export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
-// tailwind.config.js
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./App.{js,jsx,ts,tsx}', './screens/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+    content: [
+        './App.{js,jsx,ts,tsx}',
+        './screens/**/*.{js,ts,jsx,tsx}',
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
     theme: {
         extend: {},
     },
